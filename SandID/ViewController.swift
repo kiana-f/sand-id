257c8c9400c65593fbe9d3d62262e6e4a88780fd
Kiana <kiki112299@hotmail.com>
2022-01-21 11:10:34 -0600

    set up basic open camera function

--- SandID/ViewController.swift
+++ SandID/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+//	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var cameraButton: UIButton!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
 	}
+	
+	
+	@IBAction func didTapButton() {
+		print("inside didTapButton")
+		let picker = UIImagePickerController()
+		picker.sourceType = .camera
+		picker.delegate = self
+		present(picker, animated: true)
+		print("at the end of didTapButton")
+	}
+	
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		picker.dismiss(animated: true, completion: nil)
+	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		
+		//stub
+	}
 }
 

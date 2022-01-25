1b577ba344cf37ff2be5deb9b383fd3115581d1b
Kiana <kiki112299@hotmail.com>
2022-01-25 17:08:36 -0600

    custom camera working

--- SandID/ViewController.swift
+++ SandID/ViewController.swift
@@ -6,38 +6,136 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 	
-//	@IBOutlet var imageView: UIImageView!
-	@IBOutlet var cameraButton: UIButton!
+	// capture session
+	var session: AVCaptureSession?
+	
+	// photo output
+	var output = AVCapturePhotoOutput()
+	
+	// preview
+	let previewLayer = AVCaptureVideoPreviewLayer()
+	
+	// shutter button
+	private let shutterButton: UIButton = {
+		let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+		// make circular
+		button.layer.cornerRadius = 50
+		button.layer.borderWidth = 10
+		button.layer.borderColor = UIColor.white.cgColor
+		return button
+	}()
+
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		// just incase
+		view.backgroundColor = .black
+		
+		// add preview layer
+		view.layer.addSublayer(previewLayer)
+		
+		// add shutter button to screen
+		view.addSubview(shutterButton)
+		
+		checkCameraPermissions()
+		shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
+	}
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		previewLayer.frame = view.bounds
+		
+		// button shape
+		shutterButton.center = CGPoint(x: view.frame.size.width/2,
+									   y: view.frame.size.height - 100)
+	}
+	
+	// camera permissions
+	private func checkCameraPermissions() {
+		switch AVCaptureDevice.authorizationStatus(for: .video) {
+			
+		case .notDetermined:
+			// request permission
+			print("Requesting permission")
+			AVCaptureDevice.requestAccess(for: .video, completionHandler: { granted in
+				guard granted else {
+					return
+				}
+				
+				DispatchQueue.main.async {
+					self.setUpCamera()
+				}
+			})
+		case .restricted:
+			break
+			// probably throw error?
+		case .denied:
+			break
+			// probably throw error?
+		case .authorized:
+			print("Authorized")
+			setUpCamera()
+		@unknown default:
+			break
+		}
 		
 	}
 	
+	// camera input and output from device
+	private func setUpCamera() {
+		let session = AVCaptureSession()
+		if let device = AVCaptureDevice.default(for: .video) {
+			do {
+				let input = try AVCaptureDeviceInput(device: device)
+				if session.canAddInput(input) {
+					session.addInput(input)
+				}
+				
+				if session.canAddOutput(output) {
+					session.addOutput(output)
+				}
+				
+				previewLayer.videoGravity = .resizeAspectFill
+				previewLayer.session = session
+				
+				session.startRunning()
+				self.session = session
+			} catch {
+				print("ERROR")
+				print(error)
+			}
+		}
+	}
 	
-	@IBAction func didTapButton() {
-		print("inside didTapButton")
-		let picker = UIImagePickerController()
-		picker.sourceType = .camera
-		picker.delegate = self
-		present(picker, animated: true)
-		print("at the end of didTapButton")
+	// capture photo
+	@objc private func didTapTakePhoto() {
+		output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
 	}
 	
 }
 
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-		picker.dismiss(animated: true, completion: nil)
-	}
+extension ViewController: AVCapturePhotoCaptureDelegate {
 	
-	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+	// photo display after capture
+	func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+		guard let data = photo.fileDataRepresentation() else {
+			return
+		}
 		
-		//stub
+		let image = UIImage(data: data)
+		
+		session?.stopRunning()
+		
+		let imageView = UIImageView(image: image)
+		imageView.contentMode = .scaleAspectFill
+		imageView.frame = view.bounds
+		view.addSubview(imageView)
 	}
 }
 
